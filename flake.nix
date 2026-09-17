d2112db0f9bfab7296e75e50deddfd8450d7b384
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-09-17 17:23:27 +0200

    chore: advance Phenix AI dependency to extracted core head

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Phenix AI Neovim client";
 
   inputs = {
-    phenix-ai.url = "github:matthis-k/phenix-ai/f04a811d464136bf31c070c2a30dc62401daeafc";
+    phenix-ai.url = "github:matthis-k/phenix-ai/cbda1e59add04fb681b4a5b2e4154eb62c8ec19b";
     nixpkgs.follows = "phenix-ai/nixpkgs";
   };
 
